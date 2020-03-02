863961f9ec74724c1a16b60c3c499d680d3c9755
Roshan Sharma <reprogram46@gmail.com>
2020-03-02 15:27:53 +0530

    feat: Add toolbar items

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,20 @@
     <v-app-bar fixed app>
       <v-toolbar-title v-text="title" />
       <v-spacer />
+      <div>
+        <v-btn icon style="margin-right: 0.5em">
+          ABOUT
+        </v-btn>
+        <v-btn icon style="margin-right: 0.5em">
+          TEAM
+        </v-btn>
+        <v-btn icon style="margin-right: 0.5em">
+          BLOG &nbsp;
+        </v-btn>
+        <v-btn icon style="margin-right: 0.5em">
+          CONTACT
+        </v-btn>
+      </div>
     </v-app-bar>
     <v-content>
       <v-container>
