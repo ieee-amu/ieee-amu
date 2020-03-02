9ad8fe327e205efcf0d0d033056c18871653f8fb
Roshan Sharma <reprogram46@gmail.com>
2020-03-02 17:13:57 +0530

    chore: Create component to add vertical spacing

---
+++ components/Spacer.vue
@@ -0,0 +1,21 @@
+<template>
+  <section :style="{ 'margin-top': gap + '%;' }"></section>
+</template>
+
+<script>
+export default {
+  props: {
+    gap: {
+      type: String,
+      default: '5'
+    }
+  }
+}
+</script>
+
+<style scoped>
+section {
+  margin-top: 5%;
+  margin-bottom: 5%;
+}
+</style>
