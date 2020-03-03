e819d67b5b42387035435114d457c8092fe13c00
Prajjwal Nijhara <prajjwalnijhara@gmail.com>
2020-03-03 18:52:58 +0530

    Add blog.vue

---
+++ pages/blog.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card height="100%">
+        <v-img
+          class="white--text"
+          height="200px"
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        >
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">How to write a research paper</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-img>
+        <v-card-title>
+          <div>
+            <span class="grey--text">November 10</span><br />
+            <span>NL 22</span><br />
+            <span>Electrical Engg. Dept. , ZHCET, AMU</span>
+          </div>
+        </v-card-title>
+        <v-card-text>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos
+            doloribus officia, quae explicabo nulla debitis architecto eius,
+            fugit, accusantium saepe quo! Qui magnam hic ut excepturi. Hic saepe
+            nihil dolorem.
+          </p>
+          <v-img
+            class="white--text"
+            height="100px"
+            src="https://cdn.vuetifyjs.com/images/carousel/sky.jpg"
+          />
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos
+            doloribus officia, quae explicabo nulla debitis architecto eius,
+            fugit, accusantium saepe quo! Qui magnam hic ut excepturi. Hic saepe
+            nihil dolorem.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos
+            doloribus officia, quae explicabo nulla debitis architecto eius,
+            fugit, accusantium saepe quo! Qui magnam hic ut excepturi. Hic saepe
+            nihil dolorem.
+          </p>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
