a5a264c85e2425cd31c72bd4c66b9c8554788ce6
Roshan Sharma <reprogram46@gmail.com>
2020-08-05 18:47:05 +0530

    contact.vue: Set up interface for visitors
    Create form for visitors to add info to submit in database.

---
+++ pages/contact.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container mt-4>
+    <v-row justify="space-around">
+      <v-col cols="12">
+        <h1 class="text-left display-3">Reach out</h1>
+      </v-col>
+    </v-row>
+    <v-form>
+      <v-container class="mt-10">
+        <v-row justify="center">
+          <v-col cols="12" sm="4" md="3">
+            <v-text-field label="Full name"></v-text-field>
+          </v-col>
+
+          <v-col cols="12" sm="4" md="3">
+            <v-text-field label="Email address"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-col xs="12" sm="8" md="6">
+            <v-textarea outlined label="Type a message or query"></v-textarea>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </v-container>
+</template>
