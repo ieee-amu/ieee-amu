2bfc8b94fa19f780d203dfd921885f4a81ce7b3e
Roshan Sharma <reprogram46@gmail.com>
2020-08-06 15:03:25 +0530

    feat: Allow visitors to send queries to Firestore
    Use a dialog component for a success message (it only shows itself on the first message successfully posted).

---
+++ components/DialogBox.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-dialog v-model="showDialog" max-width="290">
+    <v-card>
+      <v-card-title class="headline">
+        Message sent
+      </v-card-title>
+
+      <v-card-text>
+        Your message was delivered! Keep an eye at your inbox for a response!
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn @click="closeDialog" text>OK</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      showDialog: this.show
+    }
+  },
+  methods: {
+    closeDialog() {
+      this.showDialog = false
+    }
+  }
+}
+</script>
--- pages/contact.vue
+++ pages/contact.vue
@@ -13,19 +13,74 @@
       <v-container class="mt-10">
         <v-row justify="center">
           <v-col cols="12" sm="4" md="3">
-            <v-text-field label="Full name"></v-text-field>
+            <v-text-field v-model="name" label="Full name"></v-text-field>
           </v-col>
 
           <v-col cols="12" sm="4" md="3">
-            <v-text-field label="Email address"></v-text-field>
+            <v-text-field v-model="email" label="Email address"></v-text-field>
           </v-col>
         </v-row>
         <v-row justify="center">
           <v-col xs="12" sm="8" md="6">
-            <v-textarea outlined label="Type a message or query"></v-textarea>
+            <v-textarea
+              v-model="message"
+              outlined
+              label="Type a message or query"
+            ></v-textarea>
           </v-col>
         </v-row>
       </v-container>
     </v-form>
+    <div class="text-center">
+      <v-btn @click="submit" large color="dark">
+        Send &nbsp; <v-icon>mdi-send</v-icon>
+      </v-btn>
+    </div>
+    <!-- eslint-disable-next-line vue/require-component-is -->
+    <component :is="componentName" v-bind="{ show: true }"></component>
   </v-container>
 </template>
+
+<script>
+import DialogBox from '@/components/DialogBox'
+
+export default {
+  data() {
+    return {
+      componentName: '',
+      sendSuccess: false,
+      name: '',
+      email: '',
+      message: ''
+    }
+  },
+
+  computed: {
+    formOk() {
+      return !!(this.name && this.email && this.message)
+    }
+  },
+
+  methods: {
+    submit() {
+      if (this.formOk) {
+        this.$fireStore
+          .collection('messages')
+          .add({
+            visitor: {
+              email: this.email,
+              fullName: this.name
+            },
+            message: this.message,
+            didAdminRespond: false,
+            date: new Date().toDateString()
+          })
+          .then(() => {
+            // Render dialog-box component on success
+            this.componentName = DialogBox
+          })
+      }
+    }
+  }
+}
+</script>
