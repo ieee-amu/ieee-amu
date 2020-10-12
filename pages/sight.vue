f42f43a7c0e04e61d3bfff881ff855ba14e80272
Shaista Rehan <58884649+shaista1519@users.noreply.github.com>
2020-10-12 20:33:12 +0530

    updated sight.vue (#153)
    * updated sight.vue
    
    * modified the image size
    
    * small changes

--- pages/sight.vue
+++ pages/sight.vue
@@ -1,20 +1,35 @@
 <template>
   <div>
     <div>
-      <spacer gap="3" />
       <div class="d-flex flex-column justify-space-between align-center">
         <v-img
           contain
-          src="https://firebasestorage.googleapis.com/v0/b/ieee-amu.appspot.com/o/amu-logo.png?alt=media&token=fe5c2453-4526-4934-8679-0ca61b0883f8"
-          width="250px"
-          height="250px"
+          src="https://firebasestorage.googleapis.com/v0/b/ieee-amu-7deee.appspot.com/o/IEEE%20Sight%20png.png?alt=media&token=79a11933-883d-432e-9ab6-e3bb59a825e6"
+          width="800px"
+          height="200px"
+          style="margin-bottom: 1px"
         >
         </v-img>
+        <v-chip
+          class="ma-2"
+          color="primary"
+          style="
+            font-family: 'Hammersmith One', sans-serif !important;
+            font-size: 20px;
+          "
+        >
+          Special Interest Group On Humanitarian Technology
+        </v-chip>
       </div>
       <spacer gap="3" />
       <v-container>
-        <v-card class="mx-auto" max-width="1000">
-          <v-card-text class="text--primary">
+        <v-card
+          class="mx-auto"
+          max-width="1000"
+          color="#B2EBF2"
+          style="font-family: 'Hammersmith One', sans-serif !important"
+        >
+          <v-card-text class="text--primary" style="">
             IEEE SIGHT AMU is a student affinity group whose mission is to help
             the local community through social actions and technical projects of
             a humanitarian and sustainable nature, always seeking to involve the
@@ -35,15 +50,51 @@
         </v-card>
       </v-container>
       <spacer gap="3" />
-      <div class="carosel">
-        <v-carousel hide-delimiters>
-          <v-carousel-item
-            v-for="(item, i) in items"
-            :key="i"
-            :src="item.src"
-          ></v-carousel-item>
-        </v-carousel>
-      </div>
+
+      <v-card height="700">
+        <v-tabs
+          v-model="tab"
+          height="100"
+          background-color="primary"
+          dark
+          centered
+        >
+          <v-tab
+            v-for="item in items1"
+            :key="item.tab"
+            style="
+              font-family: 'Hammersmith One', sans-serif !important;
+              font-size: 20px;
+            "
+          >
+            {{ item.tab }}
+          </v-tab>
+        </v-tabs>
+
+        <v-tabs-items v-model="tab">
+          <v-tab-item v-for="item in items1" :key="item.tab">
+            <v-card flat contain width="100%">
+              <img
+                id="sight_photo"
+                contain
+                class="center"
+                width="60%"
+                style="background-position: inherit"
+                :src="item.src"
+              />
+              <img />
+            </v-card>
+            <v-card-text
+              style="
+                font-family: 'Hammersmith One', sans-serif !important;
+                font-size: 18px;
+              "
+              >{{ item.content }}
+            </v-card-text>
+          </v-tab-item>
+        </v-tabs-items>
+      </v-card>
+
       <spacer gap="3" />
 
       <v-row justify="center">
@@ -51,14 +102,10 @@
           <div class="container">
             <div
               class="row team_cont"
-              style="justify-content: center; background-color: #e3e1e1"
+              style="justify-content: center; background-color: #b2ebf2"
             >
               <v-col cols="12" sm="12">
-                <v-card
-                  class="mx-auto"
-                  max-width="400"
-                  color="rgba(74, 74, 74, 1)"
-                >
+                <v-card class="mx-auto" max-width="400" color="primary">
                   <v-card-text
                     class="text-center white--text headline"
                     style="
@@ -101,10 +148,105 @@
           </div>
         </v-col>
       </v-row>
+
+      <v-row>
+        <v-col cols="12" sm="12">
+          <v-card-text class="text-center" style="margin: 3rem auto"
+            ><h1
+              style="
+                font-family: 'Hammersmith One', sans-serif !important;
+                text-decoration: underline;
+              "
+            >
+              Get Involved
+            </h1></v-card-text
+          >
+        </v-col>
+
+        <v-col style="margin: 0 auto" cols="12" sm="8" md="4">
+          <v-card class="mx-auto" max-width="400" height="200">
+            <v-card-title
+              style="
+                color: midnightblue;
+                font-family: 'Hammersmith One', sans-serif !important;
+                font-size: 25px;
+              "
+              >Start a Group</v-card-title
+            >
+
+            <v-card-text
+              class="text--primary"
+              style="
+                font-family: 'Hammersmith One', sans-serif !important;
+                font-size: 15px;
+              "
+            >
+              Have an idea which you would love to implement? Email us at
+              amuieee.zhcet@gmail.com
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col style="margin: 0 auto" cols="12" sm="8" md="4">
+          <v-card class="mx-auto" max-width="400" height="200">
+            <v-card-title
+              style="
+                color: midnightblue;
+                font-family: 'Hammersmith One', sans-serif !important;
+                font-size: 25px;
+              "
+              >Join a Sight Group</v-card-title
+            >
+
+            <v-card-text
+              class="text--primary"
+              style="
+                font-family: 'Hammersmith One', sans-serif !important;
+                font-size: 15px;
+              "
+            >
+              Join a desired project group and contribute? Email us at
+              amuieee.zhcet@gmail.com 
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col style="margin: 0 auto" cols="12" sm="8" md="4">
+          <v-card class="mx-auto" max-width="400" height="200">
+            <v-card-title
+              style="
+                color: midnightblue;
+                font-family: 'Hammersmith One', sans-serif !important;
+                font-size: 25px;
+              "
+              >Volunteer</v-card-title
+            >
+
+            <v-card-text
+              class="text--primary"
+              style="
+                font-family: 'Hammersmith One', sans-serif !important;
+                font-size: 15px;
+              "
+            >
+              Volunteer with us? Email us at amuieee.zhcet@gmail.com
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
     </div>
   </div>
 </template>
 <style scoped>
+#sight_photo {
+  margin-top: 1rem;
+
+  
+  width: 60%;
+
+  /* border-radius: 100%; */
+  background-size: cover;
+  height: 450px;
+  margin-bottom: 0.5rem;
+}
 #pic {
   margin-top: 20px;
   border-radius: 100%;
@@ -159,30 +301,30 @@ export default {
   data() {
     return {
       sight_team: [],
-      items: [
+      tab: null,
+      items1: [
         {
+          tab: 'Vision',
+
           src:
-            'https://firebasestorage.googleapis.com/v0/b/ieee-amu-7deee.appspot.com/o/sight-photos%2Fdoctor-870361_1920.jpg?alt=media&token=c42e73f7-027a-4f96-96e5-f2ecd649faea',
+            'https://firebasestorage.googleapis.com/v0/b/ieee-amu-7deee.appspot.com/o/sight-photos%2Fpoor-2754335.jpg?alt=media&token=a75cb7e6-fb16-4695-afd7-243455aa0300',
+
+          content:
+            'To help the local community through social actions and technical projects of a humanitarian and sustainable nature, always seeking to involve the local community in the projects so as to seek to understand a little the reality of this population and then be able to find effective and efficient solutions, obtaining mutual understanding and collaboration',
         },
         {
+          tab: 'Mission',
           src:
-            'https://firebasestorage.googleapis.com/v0/b/ieee-amu-7deee.appspot.com/o/sight-photos%2Fpoverty-1028841_1920.jpg?alt=media&token=4af98777-8409-45fd-830f-947d95b94f15',
+            'https://firebasestorage.googleapis.com/v0/b/ieee-amu-7deee.appspot.com/o/sight-photos%2Ftechnology-4256272.jpg?alt=media&token=2f52ddbc-cec0-4bde-a723-7665b0d9c882',
+          content:
+            'Our main objective is to put undergraduate students in contact with the population and their needs, as a way of using the knowledge that is passed on to us for a greater good.',
         },
         {
+          tab: 'Values',
           src:
-            'https://firebasestorage.googleapis.com/v0/b/ieee-amu-7deee.appspot.com/o/sight-photos%2Fhumanitarian-aid-939723_1280.jpg?alt=media&token=32cc5e32-bd55-440b-ab2e-3c61ae214e2d',
-        },
-        {
-          src:
-            'https://firebasestorage.googleapis.com/v0/b/ieee-amu-7deee.appspot.com/o/sight-photos%2Fpollution-4077113_1920.jpg?alt=media&token=48248fef-7879-4dd2-aa3d-a40d79b2c69d',
-        },
-        {
-          src:
-            'https://firebasestorage.googleapis.com/v0/b/ieee-amu-7deee.appspot.com/o/sight-photos%2Fsolar-pv.jpg?alt=media&token=10350cc3-9cc1-4a1e-bbf7-eac19b1fe4eb',
-        },
-        {
-          src:
-            'https://firebasestorage.googleapis.com/v0/b/ieee-amu-7deee.appspot.com/o/sight-photos%2Fpollution-4077113_1920.jpg?alt=media&token=48248fef-7879-4dd2-aa3d-a40d79b2c69d',
+            'https://firebasestorage.googleapis.com/v0/b/ieee-amu-7deee.appspot.com/o/sight-photos%2Fdoctor-870361_1920.jpg?alt=media&token=6dc6f072-a331-45d8-959e-460b6f8a2bac',
+          content:
+            'Our values are empathy, solidarity, teamwork, commitment and, of course, pride in being IEEE SIGHT.',
         },
       ],
     }
@@ -201,3 +343,4 @@ export default {
   },
 }
 </script>
+
