da43e10d12b6e4a0961f5e785009fbe3cf1e0bf9
Prajjwal Nijhara <prajjwalnijhara@gmail.com>
2020-11-15 14:33:20 +0530

    fix: Show unresponded messages (#182)
    Signed-off-by: Prajjwal Nijhara <prajjwalnijhara@gmail.com>

---
+++ pages/administrator/contactMessages.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-list>
+    <v-list-group v-for="msg in messages" :key="msg.id" no-action>
+      <template v-slot:activator>
+        <v-list-item-content>
+          <v-list-item-title>
+            <strong>{{ msg.visitor.fullName }}</strong>
+            ({{ msg.visitor.email }})
+          </v-list-item-title>
+        </v-list-item-content>
+      </template>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-subtitle v-text="msg.message"></v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list-group>
+  </v-list>
+</template>
+
+<script>
+// Not implementing pagination in Firestore queries as it would be premature
+// optimization given the traffic this site is gonna receive.
+
+export default {
+  name: 'ContactMessages',
+  props: {
+    messages: { type: Array, required: true },
+  },
+}
+</script>
