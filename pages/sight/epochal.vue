c855193d782b6e272af8ab83b55c3351ba13644d
Shaista Rehan <58884649+shaista1519@users.noreply.github.com>
2022-01-03 17:46:19 +0530

    added epochal to sight folder

---
+++ pages/sight/epochal.vue
@@ -0,0 +1,191 @@
+<template>
+  <div class="epochal" style="max-height: 1000px; margin-bottom: 800px">
+    <div class="formbanner1-component">
+      <v-container>
+        <v-row justify="center">
+          <v-col cols="12" md="8" lg="6" class="d-flex align-center">
+            <div class="text-center text-md-left">
+              <h2 class="formbanner1-title font-weight-bold">About Epochal</h2>
+              <p class="mt-2 pt-2" style="text-align: justify">
+                Epochal provides a platform for young aspiring engineers to come
+                together and be a part of a collaborative space; a society
+                (sub-section within SIGHT?) meant for learning and growing
+                together in the field of research and engineering choosing from
+                among a variety of (sub-)societies. The idea is to foster
+                younger minds of ZHCET into identifying their niche, and
+                adapting to a research environment which aligns to their career
+                goals and interests. So, we as the research section of the
+                student branch aim to create collaboration opportunities and
+                bridge the gap between hands-on knowledge and theoretical
+                knowledge by means of practical exposure.
+              </p>
+              <a
+                href="https://docs.google.com/document/d/1jSSRcRRsOXl4YCsEkXs4AjMcTSGPIpmRX4aDKU1zxq4/edit?usp=sharing"
+                target="_blank"
+                rel="noopener"
+                style="font-weight: bold"
+                >READ MORE ABOUT EPOCHAL</a
+              >
+            </div>
+          </v-col>
+          <v-col cols="12" md="8" lg="5" class="ml-auto">
+            <v-card class="mx-auto" color="#FFF8DC">
+              <v-card-title style="font-weight: bold">
+                Join Us Today
+              </v-card-title>
+              <iframe
+                src="https://docs.google.com/forms/d/e/1FAIpQLSef92WstGozBDCRfSh_2iiJmsgqEvHRxYqAPE7A3XUnt4hfBg/viewform?embedded=true"
+                height="1000"
+                style="width: 100%; height: 70vh"
+                frameborder="0"
+                allowfullscreen=""
+                title="Embedded form"
+                >Loading…</iframe
+              >
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+      <div>
+        <v-container contain height="200px">
+          <v-row style="max-height: 150">
+            <v-card-text class="text-center" style="margin: 1rem auto"
+              ><h1 style="font-family: 'Verdana', sans-serif !important">
+                Societies at Epochal
+              </h1></v-card-text
+            >
+
+            <v-col style="margin: 0 auto" cols="12" sm="8" md="4">
+              <v-card class="mx-auto card-sec" max-width="400">
+                <v-card-title class="title"
+                  >Data Science & Machine Learning Society</v-card-title
+                >
+                <v-card-text
+                  class="text--primary content"
+                  style="text-align: justify"
+                >
+                  Data has become central to our daily lives and there is
+                  growing demand for professionals with data analysis skills.
+                  Computing and data play an ever-growing role in all areas of
+                  human knowledge. Applications of Machine Learning and Data
+                  Science are now pervasive in a wide variety of businesses
+                  looking to use data effectively, as well as in government
+                  agencies, academia and health care. These applications have
+                  led to an increased need to fundamentally understand the
+                  underlying mechanisms of statistical learning on these
+                  datasets, develop new and more powerful software and hardware
+                  tools for maximizing information extraction, and new
+                  strategies for application of these processes across a wide
+                  variety of application domains. The aim of this society is to
+                  explore and understand data driven sciences, including machine
+                  learning and to perform research on their applicability in
+                  various domains. We hope to bring together AMU students across
+                  the areas of computer vision, machine learning, natural
+                  language processing, business analytics, planning & control to
+                  leverage their skillset and build systems for social good
+                  taking one step at a time.
+                </v-card-text>
+              </v-card>
+            </v-col>
+            <v-col style="margin: 0 auto" cols="12" sm="8" md="4">
+              <v-card class="mx-auto card-sec" max-width="400">
+                <v-card-title class="title"
+                  >Embedded Systems and IoT Society</v-card-title
+                >
+                <v-card-text
+                  class="text--primary content"
+                  style="text-align: justify"
+                >
+                  Most of us will casually encounter dozens of embedded systems
+                  by mid-morning each day throughout our residences, roadways
+                  and workplaces. Fundamentally, an embedded system is some
+                  combination of hardware and software that is designed for a
+                  particular function. It senses a real-world condition, does
+                  some computing, then produces output data or control of some
+                  kind. These intelligent machines are a permanent part of our
+                  global landscape, and are continuously being expanded and
+                  upgraded by a world of forward-thinking engineers and
+                  entrepreneurs. Also expected to fuel much more growth is an
+                  overarching megatrend referred to as the Internet of Things
+                  (IoT), which involves connecting more embedded systems to the
+                  internet, enabling countless human-to-machine and
+                  machine-to-machine applications ranging from home automation
+                  to security and many beyond. This society would explore
+                  various domains of in the field of intelligent systems ranging
+                  from Data Retrieval via sensors, Sensor Networks to
+                  Automation, Control Systems and Sensor Fusion. Therefore, we
+                  aim to build a community in which students can come together
+                  and brainstorm on ideas to gain insights from fellow students
+                  in developing and researching things to reality.
+                </v-card-text>
+              </v-card>
+            </v-col>
+            <v-col style="margin: 0 auto" cols="12" sm="8" md="4">
+              <v-card class="mx-auto card-sec" max-width="400">
+                <v-card-title class="title"
+                  >Web Development & Programming Society</v-card-title
+                >
+                <v-card-text
+                  class="text--primary content"
+                  style="text-align: justify"
+                >
+                  Think about it for a second. You’re currently on the Internet.
+                  On a webpage. How’d you get here? How was this website made?
+                  What codes did they use in the development process? If you’re
+                  curious about the development and structure of websites, you
+                  will find the web development track to be a highly dynamic
+                  field as the Internet evolves rapidly. At Epochal, you can
+                  become familiar with the different languages and frameworks
+                  that go into the making of a website. Make websites as
+                  projects by using HTML, CSS, JavaScript and position yourself
+                  to enter a career in high demand. Learn about the modern
+                  front-end web development technologies such as Bootstrap and
+                  React. We focus on writing syntactically correct and clean
+                  code with accessibility for a vast audience on most browsers
+                  as well as making a Responsive Design. Learn how to keep up
+                  with the standards of the Web, how to validate your code for
+                  any errors through the HTML5 validator tool. Sometimes, We
+                  also make use of the WordPress dashboard to save time and use
+                  the right theme to prepare our own website without any code!
+                  Finally, make a portfolio of your work and start your career
+                  in Web Development as a Front-End, Back-end, Full Stack
+                  Developer or as a freelancer.
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.content {
+  font-weight: 100;
+  font-size: 14px;
+}
+.card-sec {
+  background-color: cornsilk;
+}
+.title {
+  color: midnightblue;
+  font-family: 'Verdana', sans-serif !important;
+  font-weight: bold;
+  word-break: break-word;
+}
+.formbanner1-component {
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  width: 100%;
+  margin: 0;
+  background-blend-mode: hard-light;
+  max-height: 700px;
+}
+.formbanner1-title {
+  font-size: 48px;
+  line-height: 1;
+  letter-spacing: -1px;
+  margin: 20px 0 15px;
+}
+</style>
